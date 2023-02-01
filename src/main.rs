28feacf72b517144bccee624da21cd0cdd88f13e
firecrow <fire@firecrow.com>
2023-02-01 12:35:04 +0000

    adding some structs

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,25 @@ use nix::unistd::execvp;
 use std::env;
 use std::ffi::CString;
 
+/*
+const RED: i32 = 31;
+const YELLOW: i32 = 33;
+const GREEN: i32 = 32;
+
+pub struct command {
+    out_color: i32,
+    filename: CString,
+    args: Vec<CString>,
+}
+*/
+
+fn parse_incoming_command_line(args: Vec<String>) {
+    dbg!(args);
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect();
-    dbg!(args);
+    parse_incoming_command_line(args);
 
     let cmd = CString::new("ls").unwrap();
     let dir = CString::new(".").unwrap();
